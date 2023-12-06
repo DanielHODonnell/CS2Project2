a3004d97f757068c18a853df7f336b2a967c4344
DanielHODonnell <danielodonnell@unomaha.edu>
2023-12-06 14:38:19 -0600

    major edits

--- logic.py
+++ logic.py
@@ -5,6 +5,28 @@ import os
 import csv
 
 
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(pattern, email) is not None
+
+
+def is_valid_phone(phone):
+    pattern = r'^\d{10}$'
+    return re.match(pattern, phone) is not None
+
+
+def email_exists(email):
+    if not os.path.exists('voting_results.csv'):
+        return False
+
+    with open('voting_results.csv', 'r+', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if email == row[1]:
+                return True
+    return False
+
+
 class Logic(QMainWindow, Ui_vote_app):
     def __init__(self):
         self.radio_john = False
@@ -22,25 +44,6 @@ class Logic(QMainWindow, Ui_vote_app):
         self.main_ui.reset_button.clicked.connect(self.clear_input)
         self.main_ui.vote_button.clicked.connect(self.import_to_csv)
 
-    def is_valid_email(self, email):
-        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-        return re.match(pattern, email) is not None
-
-    def is_valid_phone(self, phone):
-        pattern = r'^\d{10}$'
-        return re.match(pattern, phone) is not None
-
-    def email_exists(self, email):
-        if not os.path.exists('voting_results.csv'):
-            return False
-
-        with open('voting_results.csv', 'r+', newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                if email == row[1]:
-                    return True
-        return False
-
     def next_screen(self):
         input_text1 = self.main_ui.name_text_box.text().strip()
         input_text2 = self.main_ui.email_text_box.text().strip()
@@ -55,10 +58,10 @@ class Logic(QMainWindow, Ui_vote_app):
                 if any(char.isdigit() for char in name):
                     raise ValueError('Name should not contain numbers.')
 
-                if not self.is_valid_email(email):
+                if not is_valid_email(email):
                     raise ValueError('Invalid email format.')
 
-                if not self.is_valid_phone(phone):
+                if not is_valid_phone(phone):
                     raise ValueError('Invalid phone number format.')
 
                 next_ui = lambda: self.main_ui.stackedWidget.setCurrentWidget(self.main_ui.page_2)
@@ -89,7 +92,7 @@ class Logic(QMainWindow, Ui_vote_app):
                 choice = 'Jane' if not self.radio_jane else 'John'
                 info_list = [name_input, email_input, phone_input, choice]
 
-                if self.email_exists(email_input):
+                if email_exists(email_input):
                     raise ValueError('You have already voted.')
 
                 with open('voting_results.csv', 'a', newline='') as csvfile:
